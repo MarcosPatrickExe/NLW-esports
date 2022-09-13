f52859a5d84ac53acc31599383b3f5d1c31581a1
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-09-12 22:23:44 -0300

    Setup inicial do backend...

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// MODO 'commonjs'
+//const express = require('express');
+//liberou suporta ao ECMA script modules
+import express from 'express';
+const app = express();
+app.get('/ads', (request, response) => {
+    console.log("Acessou /Ads"); //imprime no servidor
+    //  return response.send("Acessou /Ads!!"); // imprime no browser
+    return response.json([
+        { id: 1, name: 'Anuncio 1' },
+        { id: 2, name: 'Anuncio 2' },
+        { id: 3, name: 'Anuncio 3' }
+    ]);
+});
+app.listen(3333); // NAO DEIXA A APLICACAO PARAR
+console.log("Server is running on port 'localhost:3333/ads'....");
+// HOPPSCOTCH 
+// FERRAMENTA ONLINE QUE FAZ REQUISICOES PARA OUTROS APPS BACKENDS
+// LINK DO NOTION:
+// https://efficient-sloth-d85.notion.site/NLW-eSports-30d87fcefce04116896ca1a31921401c
+// LINK DA AULA:
+// https://app.rocketseat.com.br/event/nlw-09/ignite/aula-1
